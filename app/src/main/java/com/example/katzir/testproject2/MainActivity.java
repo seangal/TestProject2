bd40b29964a0ffd5b1ab20d6b00319dedc148e00
seangal <sean1815@gmail.com>
2015-10-28 13:13:15 +0200

    add button!

--- app/src/main/java/com/example/katzir/testproject2/MainActivity.java
+++ app/src/main/java/com/example/katzir/testproject2/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.katzir.testproject2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button = (Button) findViewById(R.id.button);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void Click(View view){
+        button.setText("Thank you!");
     }
 }
