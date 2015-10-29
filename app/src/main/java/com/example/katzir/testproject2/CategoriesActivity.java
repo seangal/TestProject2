87b527e59fe10065f079cbfd3cc32c04deedb284
seangal <sean1815@gmail.com>
2015-10-29 16:58:51 +0200

    add list view

--- app/src/main/java/com/example/katzir/testproject2/CategoriesActivity.java
+++ app/src/main/java/com/example/katzir/testproject2/CategoriesActivity.java
@@ -1,13 +1,18 @@
 package com.example.katzir.testproject2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
-public class CategoriesActivity extends AppCompatActivity {
+public class CategoriesActivity extends ListActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        String[] strings = {"1","2","3"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getListView().getContext(),android.R.layout.simple_list_item_1,strings);
+        getListView().setAdapter(adapter);
     }
 }
