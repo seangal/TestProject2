1066fce9bfc9e537751657b3e882170ab9323bc4
seangal <sean1815@gmail.com>
2015-10-29 20:18:51 +0200

    clickable

--- app/src/main/java/com/example/katzir/testproject2/CategoriesActivity.java
+++ app/src/main/java/com/example/katzir/testproject2/CategoriesActivity.java
@@ -2,9 +2,12 @@ package com.example.katzir.testproject2;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class CategoriesActivity extends ListActivity {
+public class CategoriesActivity extends ListActivity implements AdapterView.OnItemClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +17,10 @@ public class CategoriesActivity extends ListActivity {
         String[] strings = {"1","2","3"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getListView().getContext(),android.R.layout.simple_list_item_1,strings);
         getListView().setAdapter(adapter);
+    }
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        // TODO print selected
+        parent.getItemAtPosition(position);
     }
 }
