c602898a3c805426c4ba4041d122ea31cbbdc9f6
seangal <sean1815@gmail.com>
2015-10-29 16:20:48 +0200

    Added expandable list view Added expandable list view adapter Added categories class

--- app/src/main/java/com/example/katzir/testproject2/MainActivity.java
+++ app/src/main/java/com/example/katzir/testproject2/MainActivity.java
@@ -4,16 +4,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ExpandableListView;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private ExpandableListView expandableListView;
 
+    private Categories categories;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        expandableListView = (ExpandableListView)findViewById(R.id.expandableListView);
+        categories=new Categories();
+        ExpandableListViewAdapter expandableListViewAdapter = new ExpandableListViewAdapter(getApplicationContext(), categories.parentHeaderInformation, categories.allChildItems);
+
+        expandableListView.setAdapter(expandableListViewAdapter);
     }
-    public void Click(View view){
-        Button button =  (Button) findViewById(R.id.button);
-        button.setText("Thank you!");
-    }
+
+
 }
