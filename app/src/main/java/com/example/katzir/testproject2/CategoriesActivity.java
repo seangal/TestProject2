13de138a8aee99ebc96fdeb2b29db0d4544eacec
Adi1231234 <adi1231234@gmail.com>
2015-10-29 21:37:23 +0200

    שינוי שם

--- app/src/main/java/com/example/katzir/testproject2/CategoriesActivity.java
+++ app/src/main/java/com/example/katzir/testproject2/CategoriesActivity.java
@@ -17,14 +17,13 @@ public class CategoriesActivity extends ListActivity {
 
         setContentView(R.layout.activity_main);
 
-        String[] strings = {"1","2","3"};
+        String[] strings = {"adi","sean","renan","jonatan"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getListView().getContext(), android.R.layout.simple_list_item_1, strings);
         getListView().setAdapter(adapter);
         getListView().setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position,
                                     long id) {
-
                 Log.d("list", parent.getItemAtPosition(position).toString());
 
             }
