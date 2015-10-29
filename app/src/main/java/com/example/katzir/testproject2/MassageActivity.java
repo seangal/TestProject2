21a20b907332708571537a0b7df8c22067db4372
seangal <sean1815@gmail.com>
2015-10-29 22:10:33 +0200

    new activity

---
+++ app/src/main/java/com/example/katzir/testproject2/MassageActivity.java
@@ -0,0 +1,13 @@
+package com.example.katzir.testproject2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MassageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_massage);
+    }
+}
