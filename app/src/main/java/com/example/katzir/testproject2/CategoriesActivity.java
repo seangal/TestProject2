ec089220034cecb431f7f086752fe56e5ebcdeed
seangal <sean1815@gmail.com>
2015-10-30 08:43:52 +0200

    removed imports

--- app/src/main/java/com/example/katzir/testproject2/CategoriesActivity.java
+++ app/src/main/java/com/example/katzir/testproject2/CategoriesActivity.java
@@ -3,13 +3,9 @@ package com.example.katzir.testproject2;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class CategoriesActivity extends ListActivity {
     @Override
