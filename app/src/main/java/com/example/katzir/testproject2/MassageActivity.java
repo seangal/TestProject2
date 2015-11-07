a7c58bb5c11c9e701b0f3a32c95c730701670a8c
seangal <sean1815@gmail.com>
2015-11-07 14:38:44 +0200

    add listview

--- app/src/main/java/com/example/katzir/testproject2/MassageActivity.java
+++ app/src/main/java/com/example/katzir/testproject2/MassageActivity.java
@@ -4,13 +4,36 @@ import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MassageActivity extends AppCompatActivity {
-    TextView textView;
+    ListView list;
+    String[] web = {
+            "Google Plus",
+            "Twitter",
+            "Windows",
+            "Bing",
+            "Itunes",
+            "Wordpress",
+            "Drupal"
+    } ;
+    Integer[] imageId = {
+            R.drawable.image1,
+            R.drawable.image2,
+            R.drawable.image3,
+            R.drawable.image4,
+            R.drawable.image5,
+            R.drawable.image6,
+            R.drawable.image7
+
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_massage);
         // get the intent from which this activity is called.
@@ -18,5 +41,20 @@ public class MassageActivity extends AppCompatActivity {
 
         // fetch value from key-value pair and make it visible on TextView.
         String item = intent.getStringExtra("selected-item");
+
+        CustomList adapter = new
+                CustomList(MassageActivity.this, web, imageId);
+        list=(ListView)findViewById(R.id.list);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Toast.makeText(MassageActivity.this, "You Clicked at " + web[+position], Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 }
