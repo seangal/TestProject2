79d51a4de51836c3083aee77ca99195c85c3c63f
renanbazinin <renanbazinin2@gmail.com>
2015-11-07 15:36:59 +0200

    GETHTTP :D ♥

--- app/src/main/java/com/example/katzir/testproject2/MassageActivity.java
+++ app/src/main/java/com/example/katzir/testproject2/MassageActivity.java
@@ -10,6 +10,15 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+
+
+
 public class MassageActivity extends AppCompatActivity {
     ListView list;
     String[] web = {
@@ -29,6 +38,7 @@ public class MassageActivity extends AppCompatActivity {
             R.mipmap.image5,
             R.mipmap.image6,
             R.mipmap.image7
+
     };
 
     @Override
@@ -55,5 +65,20 @@ public class MassageActivity extends AppCompatActivity {
             }
         });
 
+
+
+    }
+    public static String getHTML(String urlToRead) throws Exception {
+        StringBuilder result = new StringBuilder();
+        URL url = new URL(urlToRead);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line;
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+        }
+        rd.close();
+        return result.toString();
     }
 }
